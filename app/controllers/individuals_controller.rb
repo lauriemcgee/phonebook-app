7b2f2d3df374220d4b16e81c5bee550154bf0934
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-15 21:05:03 -0500

    index, routes generated

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -1,2 +1,22 @@
 class IndividualsController < ApplicationController
+  def index
+    @contacts = Individual.all
+    return "index.html.erb"
+  end
+  def new
+    return "new.html.erb"
+  end
+  def create
+
+  end
+  def show
+    return "show.html.erb"
+  end
+  def edit
+    return "edit.html.erb"
+  end
+  def update
+  end
+  def destroy
+  end
 end
